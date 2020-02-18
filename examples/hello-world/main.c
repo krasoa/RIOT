5c69eed085d6e964a8895ece14a73964f43567fc
Aleks Krasowski <a.krasowski@fu-berlin.de>
2020-02-18 15:22:34 +0100

    NRF_POWER->SYSTEMOFF=1

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -19,14 +19,11 @@
  * @}
  */
 
-#include <stdio.h>
+#include <vendor/nrf52.h>
 
 int main(void)
 {
-    puts("Hello World!");
-
-    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
-    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+    NRF_POWER->SYSTEMOFF=1;
 
     return 0;
 }
